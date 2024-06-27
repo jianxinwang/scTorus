75ad5b718c246594a583a855100545f3ac0a0408
Jianxin Wang <jianxinwang24@gmail.com>
2024-06-27 13:30:11 -0400

    updated data files

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,17 +6,14 @@ library(data.table)
 library(dplyr)
 
 # BCM data
-setwd("/mnt/Vectra/Vectra Projects/Vectra Projects_ Witkiewicz.Knudsen Lab/2023_9 EK Baylor TMA/Consolidated for Jason/")
+
+setwd("/mnt/Vectra/Vectra_Projects/Vectra_Projects_Witkiewicz.Knudsen_Lab/2023.9 EK Baylor TMA/Consolidated for Jason/")
 
 data.bcm.cc2.09.1 <- fread("Consolidated_data_BCM09-1.txt")
-data.bcm.cc2.09.2 <- fread("Consolidated_data_BCM09-2.txt")
-data.bcm.cc2.10.1 <- fread("Consolidated_data_BCM10-1.txt")
 data.bcm.cc2.10.2 <- fread("Consolidated_data_BCM10-2.txt")
 
 tmpList <- list()
 tmpList[['bcm.cc2.09.1']] <- data.bcm.cc2.09.1
-tmpList[['bcm.cc2.09.2']] <- data.bcm.cc2.09.2
-tmpList[['bcm.cc2.10.1']] <- data.bcm.cc2.10.1
 tmpList[['bcm.cc2.10.2']] <- data.bcm.cc2.10.2
 
 dataSetList <- list()
@@ -28,7 +25,7 @@ for (ds in names(tmpList)){
     
     # get the mean value columns
     tmp <- tmp %>% dplyr::select(contains(c('Sample Name', 'Cell ID', 'Mean'))) %>% 
-        dplyr::select(-contains(c('AE1AE3', 'Autofluorescence', 'Nucleus', 'Membrane', 'Cytoplasm')))
+        dplyr::select(-contains(c('AE1AE3', 'Autofluorescence', 'Entire', 'Membrane', 'Cytoplasm')))
 
     
     # clean column names
